80c6b514c7153433cae5fe02b817646ef8a485a8
Kelly Davis <kldavis4@gmail.com>
2025-05-11 09:10:40 -0400

    commit config

--- tina/config.tsx
+++ tina/config.tsx
@@ -6,6 +6,7 @@ import Author from "./collection/author";
 import Page from "./collection/page";
 
 const config = defineConfig({
+  //oauth2: true,
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
   branch:
     process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
@@ -31,23 +32,23 @@ const config = defineConfig({
   schema: {
     collections: [Page, Post, Author, Global],
   },
-   tinaioConfig: {
-    //frontendUrlOverride: 'https://pr1993-app.tinajs.dev', // 'https://app.tina.io'
-    //identityApiUrlOverride: 'https://pr1993-identity.tinajs.dev',
-    //contentApiUrlOverride: 'https://pr1993-content.tinajs.dev',
-    //assetsApiUrlOverride: 'https://assets.tinajs.io',
-    //frontendUrlOverride: 'https://app.tina.io', // 'https://app.tina.io'
-    //identityApiUrlOverride: 'https://identity.tinajs.io',
-    //contentApiUrlOverride: 'https://content.tinajs.io',
-    //assetsApiUrlOverride: 'https://assets.tinajs.dev',
-    //frontendUrlOverride: 'https://app.tinajs.dev', // 'https://app.tina.io'
-    //identityApiUrlOverride: 'https://identity.tinajs.dev',
-    //contentApiUrlOverride: 'https://content.tinajs.dev'
-    frontendUrlOverride: 'http://localhost:3002', // 'https://app.tina.io'
-    identityApiUrlOverride: 'https://kldavis4-identity.tinajs.dev',
-    contentApiUrlOverride: 'https://kldavis4-content.tinajs.dev',
-    //assetsApiUrlOverride: 'https://kldavis4-assets.tinajs.dev'
-    //assetsApiUrlOverride: 'https://assets-api-local-kldavis4.tinajs.dev'
+  tinaioConfig: {
+  //   //frontendUrlOverride: 'https://pr1993-app.tinajs.dev', // 'https://app.tina.io'
+  //   //identityApiUrlOverride: 'https://pr1993-identity.tinajs.dev',
+  //   //contentApiUrlOverride: 'https://pr1993-content.tinajs.dev',
+  //   //assetsApiUrlOverride: 'https://assets.tinajs.io',
+  //   //frontendUrlOverride: 'https://app.tina.io', // 'https://app.tina.io'
+  //   //identityApiUrlOverride: 'https://identity.tinajs.io',
+  //   //contentApiUrlOverride: 'https://content.tinajs.io',
+  //   //assetsApiUrlOverride: 'https://assets.tinajs.dev',
+  //   //frontendUrlOverride: 'https://app.tinajs.dev', // 'https://app.tina.io'
+  //   //identityApiUrlOverride: 'https://identity.tinajs.dev',
+  //   //contentApiUrlOverride: 'https://content.tinajs.dev'
+       frontendUrlOverride: 'http://localhost:3002', // 'https://app.tina.io'
+       identityApiUrlOverride: 'https://kd-identity.tinajs.dev',
+       contentApiUrlOverride: 'https://kd-content.tinajs.dev',
+  //   //assetsApiUrlOverride: 'https://kldavis4-assets.tinajs.dev'
+  //   //assetsApiUrlOverride: 'https://assets-api-local-kldavis4.tinajs.dev'
   },
 });
 
