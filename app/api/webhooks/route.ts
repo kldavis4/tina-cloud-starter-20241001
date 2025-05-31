83b024768aa1cbb6792cabffba8a92e84c7e401e
Kelly Davis <kldavis4@gmail.com>
2025-05-30 21:06:29 -0400

    update to support paths

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -4,16 +4,16 @@ import { revalidateTag } from 'next/cache';
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
-        const path = body.path;
+        const paths = body.paths;
 
-        if (!path) {
-            return NextResponse.json({ error: 'Path field is required' }, { status: 400 });
+        if (!paths) {
+            return NextResponse.json({ error: 'Paths field is required' }, { status: 400 });
         }
 
         // Call revalidateTag with the path value
-        revalidateTag(path);
+        revalidateTag(paths);
 
-        return NextResponse.json({ message: `revalidation triggered for path: ${path}`, payload: body });
+        return NextResponse.json({ message: `revalidation triggered for path: ${paths}`, payload: body });
     } catch (error) {
         console.error('Error processing request:', error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
